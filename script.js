6ef857b008c78eb534bb7d21d67fe148e424b777
ginarose <ginapiscitelli@gmail.com>
2014-07-23 20:50:47 -0400

    Added to do list

--- script.js
+++ script.js
@@ -16,5 +16,16 @@ $(document).ready(function(){
 					displayname = data.user.username._content;
 					}
 		});
+
+  			// T O  D O
+		//use user id to get public photos and add photo ids to array
+		//https://www.flickr.com/services/api/explore/flickr.people.getPublicPhotos
+
+		//loop through array to get exif data and camera information
+		//https://www.flickr.com/services/api/explore/flickr.photos.getExif
+
+		//make dropdown list of cameras
+
+		//when camera is selected, create photo gallery of matching photos in user's uploads
 	})
 });
