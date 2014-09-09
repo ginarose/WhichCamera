29474b9d1905b5133b7048abbf485e8d0b28fc26
ginarose <ginapiscitelli@gmail.com>
2014-09-09 00:41:00 -0400

    Added display of username and flickrID, don't know why it wasn't there before

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
   	//takes in the username from user input
   		var usernameinput = $('#username').val();
   	//formats it in the flickr url needed for grabbing data
-  		var userinfo = "https://api.flickr.com/services/rest/?method=flickr.urls.lookupUser&api_key=3e1bfcd8270761bbc1c9d7849cf71876&url=www.flickr.com%2Fphotos%2F"+usernameinput+"%2F&format=json&jsoncallback=?";
+  		var userinfo = "https://api.flickr.com/services/rest/?method=flickr.urls.lookupUser&api_key=LALALAAPI&url=www.flickr.com%2Fphotos%2F"+usernameinput+"%2F&format=json&jsoncallback=?";
   		console.log(userinfo);
 
   		$.ajax({
@@ -14,6 +14,7 @@ $(document).ready(function(){
 					//pulls the movie poster
 					flickrID = data.user.id;
 					displayname = data.user.username._content;
+					$('#testing-output').text("Your username is "+displayname+" and your flickrID is "+flickrID+"!");
 					}
 		});
 
